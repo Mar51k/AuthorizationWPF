cc575f7f8215c115e92a7996e8dbfef604e98203
kawaragi <dima.kretinin.05@mail.ru>
2024-12-07 15:22:00 +0700

    fixed some bugs

--- Authorization/Pages/Client.xaml.cs
+++ Authorization/Pages/Client.xaml.cs
@@ -30,36 +30,29 @@ namespace Authorization.Pages
         public Client(Authtorizations user ,string role)
         {
             InitializeComponent();
-            _user = user;
-            _role = role;
-            NameTake();
-            TimeCheck();
-
-            if (role != null) 
+            if (DateTime.Now.Hour >= 19 && DateTime.Now.Minute > 0)
             {
-                _role = "Гость";
-                TextBlock_.Text = $"Вы вошли как {role}";
-                while (true)
-                {
-                    if (DateTime.Now.Hour >= 19 && DateTime.Now.Minute > 0)
-                    {
-                        MessageBox.Show("Смена закончена!");
-                        NavigationService.GoBack();
-                    }
-                }
+                MessageBox.Show("Смена закончена!");
+                NavigationService.GoBack();
             }
             else
             {
-                TextBlock_.Text = "Вы вошли как пользователь!";
-                while (true)
+                _user = user;
+                _role = role;
+                NameTake();
+                TimeCheck();
+
+                if (role != null)
                 {
-                    if (DateTime.Now.Hour >= 19 && DateTime.Now.Minute > 0)
-                    {
-                        MessageBox.Show("Смена закончена!");
-                        NavigationService.GoBack();
-                    }
+                    _role = "Гость";
+                    TextBlock_.Text = $"Вы вошли как {role}";
+                }
+                else
+                {
+                    TextBlock_.Text = "Вы вошли как пользователь!";
                 }
             }
+           
         }
 
         private void TextBlock_GiveFeedback(object sender, GiveFeedbackEventArgs e)
@@ -69,42 +62,42 @@ namespace Authorization.Pages
 
         private void TimeCheck()
         {
-            while (true)
-            {
-                DateTime now = DateTime.Now;
+            DateTime now = DateTime.Now;
 
-                if (now.Hour >= 10 || now.Hour <= 12)
-                {
-                    Time.Text = $"Доброе утро! {gender} {name}";
-                }
-                else if ((now.Hour >= 12 && now.Minute >= 1) || now.Hour <= 17)
-                {
-                    Time.Text = $"Добрый день! {gender} {name}";
-                }
-                else if ((now.Hour >= 17 && now.Minute >= 1) || now.Hour <= 19)
-                {
-                    Time.Text = $"Добрый вечер! {gender} {name}";
-                }
+            if (now.Hour >= 10 && now.Hour <= 12)
+            {
+                Time.Text = $"Доброе утро! {gender} {name}";
             }
-            
+            else if ((now.Hour >= 12 && now.Minute >= 1) && now.Hour <= 17)
+            {
+                Time.Text = $"Добрый день! {gender} {name}";
+            }
+            else if ((now.Hour >= 17 && now.Minute >= 1) && now.Hour <= 19)
+            {
+                Time.Text = $"Добрый вечер! {gender} {name}";
+            }            
         }
 
         private void NameTake()
         {
             construction_organizationEntities db = Helper.GetContext();
-            var user = db.Users.Where(x => x.id == _user.user_id).FirstOrDefault();
-            if (user != null)
+            var usernow = db.Users.Where(x => x.id == _user.user_id).FirstOrDefault();
+            if (usernow != null)
             {
-                if (user.name[user.name.Length - 1].ToString() == "a")
+                if (usernow.name[usernow.name.Length - 1].ToString() == "a" || usernow.name[usernow.name.Length - 1].ToString() == "я")
                 {
                     gender = "Mrs";
-                    name = user.name;
+                    name = usernow.name;
                 }
                 else
                 {
                     gender = "Mr";
-                    name = user.name;
+                    name = usernow.name;
                 }
+            }
+            else 
+            {
+                MessageBox.Show("Ошибка!");
             }
         }
     }
